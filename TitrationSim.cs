e40f2e9ae40580548acfdedd014a37638d65a9ab
damire <damiomabuwa177@gmail.com>
2026-04-30 13:29:30 -0500

    Add files via upload

---
+++ TitrationSim.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace;
+
+public class TitrationSim
+{
+    //Strong acid - strong base
+    // HCl(25 mL, .0055 M HCl)  + NaOH(.10 M, needs 55 mL) 
+    
+    //Weak acid - strong base
+    // 
+    
+    
+}
