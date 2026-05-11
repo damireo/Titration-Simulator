f5175ec422579b6bc262f6885921b71aee4384b9
annaholmes6 <annakhsports@gmail.com>
2026-05-10 21:11:52 -0500

    Set up titration simulation project and fix compilation errors

--- obj/Debug/net7.0/TitrationSim.AssemblyInfo.cs
+++ obj/Debug/net7.0/TitrationSim.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TitrationSim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a28c0d1b5279021b170a2823a0803b965434a071")]
 [assembly: System.Reflection.AssemblyProductAttribute("TitrationSim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TitrationSim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
